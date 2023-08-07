2478f067cea72bd81dff4711a7606b3685241d6d
Henry Lee <henry.lee@griffithuni.edu.au>
2023-08-07 11:29:01 +1000

    Add EnemySpawner.cs

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private List<Transform> spawnPoints;
+    [SerializeField] private int maxEnemies;
+    [SerializeField] private float spawnDelay;
+
+    [SerializeField] private bool enabled;
+    private float currentDelay;
+
+    private void Start() {
+        currentDelay = spawnDelay;
+    }
+    private void Update() {
+        if (!enabled) return;
+        currentDelay -= Time.deltaTime;
+        if (currentDelay <= 0) {
+            currentDelay = spawnDelay;
+            Instantiate(enemyPrefab, spawnPoints[Random.Range(0, spawnPoints.Count-1)]);
+        }
+        
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.cyan;
+        foreach (Transform t in spawnPoints) {
+            Gizmos.DrawSphere(t.position, 0.5f);
+        }
+    }
+}
