4a4080a8a61a22c13f08870a26a0422f2107cc11
Henry Lee <henry.lee@griffithuni.edu.au>
2023-08-07 16:49:48 +1000

    Modified PlayerManager.cs to use new Interact

--- Assets/Scripts/Prototype/PlayerManager.cs
+++ Assets/Scripts/Prototype/PlayerManager.cs
@@ -1,13 +1,13 @@
+using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerManager : MonoBehaviour
 {
     GameManager gameManager;
-
-    private int currentHealth;
 
     [SerializeField] private GameObject bullet;
     [SerializeField] private Transform cameraRoot;
@@ -16,37 +16,34 @@ public class PlayerManager : MonoBehaviour
     [SerializeField]    [Range(0f, 5f)] private float interactRange;
 
 
-
+    private StarterAssetsInputs _input;
 
     void Start()
     {
         gameManager = GameManager.Instance;
+        _input = GetComponent<StarterAssetsInputs>();
     }
 
     void Update()
     {
-        RaycastHit hit;
-        if (Input.GetKeyDown(KeyCode.E)) {
+        if (_input.interact) {
+            RaycastHit hit;
+            _input.interact = false;
             if (Physics.Raycast(cameraRoot.position, cameraRoot.forward, out hit, interactRange, interactMask)) {
                 Debug.Log("Got Hit");
-                
+
                 if (hit.collider.TryGetComponent(out Interactable interactable)) {
                     interactable.interact(gameObject);
                 }
             }
         }
-/*        if (Input.GetKeyDown(KeyCode.V)) {
-            if (Time.timeScale == 1f) {
-                Time.timeScale = 0.1f;
-            } else {
-                Time.timeScale = 1f;
-            }
-        }*/
     }
 
 
 
     private void OnDrawGizmos() {
+
+        // Interaction Probe
         Gizmos.color = Color.green;
         Gizmos.DrawLine(cameraRoot.position, cameraRoot.position + cameraRoot.forward * interactRange);
     }
