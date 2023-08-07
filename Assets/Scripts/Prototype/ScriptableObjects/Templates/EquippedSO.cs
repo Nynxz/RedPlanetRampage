3912e7c82678cea10c17d75c31f2a05960a831a5
Henry Lee <henry.lee@griffithuni.edu.au>
2023-08-07 11:21:37 +1000

    Add EquippedSO.cs
    Weapon Wrapper Scriptable Object Template

---
+++ Assets/Scripts/Prototype/ScriptableObjects/Templates/EquippedSO.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquippedSO : ScriptableObject
+{
+    public WeaponSO weaponSO;
+    private int currentAmmoInMag;
+    private int totalAmmoLeft;
+
+
+    public void Setup() {
+        currentAmmoInMag = weaponSO.ammoCount;
+        totalAmmoLeft = weaponSO.ammoCount * weaponSO.magCount - currentAmmoInMag;
+    }
+
+    public bool CanShoot() {
+        if (currentAmmoInMag > 0) {
+            return true;
+        } else {
+            Debug.Log("Out Of Ammo In Mag");
+            return false;
+        }
+    }
+    public void Shoot() {
+        if (currentAmmoInMag > 0) {
+            currentAmmoInMag--;
+        } else {
+            //Reload();
+        }
+    }
+    public void Reload() {
+        if(totalAmmoLeft <= 0) {
+            Debug.Log("No Ammo Left");
+        }else if (weaponSO.ammoCount - currentAmmoInMag <= totalAmmoLeft) {
+            totalAmmoLeft -= weaponSO.ammoCount - currentAmmoInMag;
+            currentAmmoInMag = weaponSO.ammoCount;
+        } else { // Reload as much as we can
+            currentAmmoInMag = totalAmmoLeft;
+            totalAmmoLeft = 0;
+        }
+    }
+
+    public GameManager.UpdateAmmoArgs GetAmmoArgs() {
+        return new GameManager.UpdateAmmoArgs() {
+            currentInMag = currentAmmoInMag,
+            maximumInMag = weaponSO.ammoCount,
+            ammoLeft = totalAmmoLeft
+        };
+    }
+
+    public void FillAmmo() {
+        Setup();
+    }
+}
