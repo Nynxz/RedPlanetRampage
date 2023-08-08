6ea580a6f298ab548cfc4914835a488042f8743f
Henry Lee <henry.lee@griffithuni.edu.au>
2023-08-08 15:32:07 +1000

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,30 +4,29 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
+
+[RequireComponent(typeof(UIManager))]
+[RequireComponent(typeof(InputManager))]
+public class GameManager : MonoBehaviour {
     // Singleton Instance
     public static GameManager Instance;
 
+
+    // Components Needed
+    public UIManager UIManager { get; private set; }
+    public InputManager InputManager { get; private set; }
+
+
+
+    //Testing Branch
     [SerializeField] public Transform playerTransform;
 
-    // Inspector Fields
-    [SerializeField] private TextMeshProUGUI moneyText;
-    [SerializeField] private TextMeshProUGUI scoreText;
-    [SerializeField] private TextMeshProUGUI ammoText;
 
-
-    [SerializeField] public Canvas UICanvas;
-    [SerializeField] public GameObject hitmarkerRegular;
-    [SerializeField] public GameObject hitmarkerHeadshot;
 
     // Player Variables
     private int currentMoney = 0;
     private int currentScore = 0;
 
-    // String Decorators
-    private readonly string moneyPrefix = "$ ";
-    private readonly string scorePrefix = "Score: ";
 
     // Player Variable Update Events
     public class UpdateAmmoArgs {
@@ -52,28 +51,16 @@ public class GameManager : MonoBehaviour
             Debug.LogWarning("Multiple instances of GameManager.cs found, Please only use one.");
         } else {
             Instance = this;
+            UIManager = GetComponent<UIManager>();
+            InputManager = GetComponent<InputManager>();
         }
     }
 
     void Start()
     {
         // When these events are 'invoked' call the SetText Method
-        UpdateMoney += SetMoneyText;
-        UpdateScore += SetScoreText;
-    }
-
-    // These are called when invoking the Events
-    private void SetMoneyText(object sender, UpdateMoneyEventArgs e) {
-        moneyText.text = moneyPrefix + e.moneyAmount.ToString();
-    }
-    private void SetScoreText(object sender, UpdateScoreEventArgs e) {
-        scoreText.text = scorePrefix + e.scoreAmount.ToString();
-    }
-    public void SetAmmoText(UpdateAmmoArgs e) {
-        ammoText.text = e.currentInMag.ToString() + "/" + e.maximumInMag.ToString() + " | " + e.ammoLeft.ToString();
-    }
-    public void SetAmmoText(string text) {
-        ammoText.text = text;
+        UpdateMoney += UIManager.SetMoneyText;
+        UpdateScore += UIManager.SetScoreText;
     }
 
     // These are the functions you want to use to update the score and money
