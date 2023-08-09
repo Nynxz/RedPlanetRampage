d10dd1755e46d92e3eeaafa1920149b297a74e07
Henry Lee <henry.lee@griffithuni.edu.au>
2023-08-09 13:06:50 +1000

    Create Player.cs

---
+++ Assets/Player.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] public Transform cameraRoot;
+    [SerializeField] public LayerMask interactMask;
+    [SerializeField] public float interactRange;
+
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
