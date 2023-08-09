d8a46f4905d0f0fea6347faddf13fd22dc9455e8
Henry Lee <henry.lee@griffithuni.edu.au>
2023-08-09 14:16:24 +1000

    Updated test enemy
    Does Damage

--- Assets/Scripts/Prototype/EnemyTest.cs
+++ Assets/Scripts/Prototype/EnemyTest.cs
@@ -7,20 +7,40 @@ public class EnemyTest : MonoBehaviour
 {
     [SerializeField] private float startingHealth;
     [SerializeField] private RectTransform healthBarRect;
-    
+
+    [SerializeField] private float attackDamage;
+    [SerializeField] private float attackRange;
+    [SerializeField] private float attackCooldown;
+    private float currentAttackCooldown = 0;
+
+
     private float currentHealth;
     private NavMeshAgent agent;
-    
-    void Start()
-    {
+
+    void Start() {
         currentHealth = startingHealth;
         healthBarRect.localScale.Set(1f, 1f, 1f);
         agent = GetComponent<NavMeshAgent>();
-        agent.SetDestination(GameManager.Instance.playerTransform.position);
+        agent.SetDestination(GameManager.Instance.PlayerManager.PlayerPosition);
     }
 
     private void Update() {
-        agent.SetDestination(GameManager.Instance.playerTransform.position);
+        agent.SetDestination(GameManager.Instance.PlayerManager.PlayerPosition);
+        if(Vector3.Distance(transform.position, GameManager.Instance.PlayerManager.PlayerPosition) <= attackRange) {
+            Attack();
+        }
+
+        if(currentAttackCooldown <= attackCooldown) {
+            currentAttackCooldown += Time.deltaTime;
+        }
+    }
+
+    public void Attack() {
+        if(currentAttackCooldown >= attackCooldown) {
+            currentAttackCooldown = 0;
+            GameManager.Instance.PlayerManager.GetPlayer.Damage(attackDamage);
+            Debug.Log("Attacking player for " + attackDamage + " damage");
+        }
     }
 
     public void Damage(float amount) {
@@ -43,4 +63,8 @@ public class EnemyTest : MonoBehaviour
         healthBarRect.localScale = new Vector3(normalizedHealth, 1f, 1f);
     }
 
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
 }
