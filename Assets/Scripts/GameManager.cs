3a381a4aa60c0cd325b8c0b73e562e4c9ec8a1e6
Henry Lee <henry.lee@griffithuni.edu.au>
2023-08-09 19:02:13 +1000

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,6 @@
 using System;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(UIManager))]
 [RequireComponent(typeof(InputManager))]
@@ -16,16 +16,42 @@ public class GameManager : MonoBehaviour {
     public InputManager InputManager { get; private set; }
     public PlayerManager PlayerManager { get; private set; }
 
+    public static string previousScene;
+    public static string currentScene;
+
 
     private void Awake() {
+        GameObject.DontDestroyOnLoad(this);
+
         // Set Singleton Instance to First Created Instance
         if (Instance != null) {
             Debug.LogWarning("Multiple instances of GameManager.cs found, Please only use one.");
         } else {
+            Debug.Log("Initing GameManager");
             Instance = this;
             UIManager = GetComponent<UIManager>();
             InputManager = GetComponent<InputManager>();
             PlayerManager = GetComponent<PlayerManager>();
+            SceneManager.activeSceneChanged += SceneManager_activeSceneChanged;
         }
     }
+
+    private void SceneManager_activeSceneChanged(Scene current, Scene next) {
+        if (current.name == null) {
+            Debug.Log("Current Scene deleted");
+            previousScene = currentScene;
+            currentScene = next.name;
+        } else {
+            previousScene = currentScene;
+            currentScene = next.name;
+
+        }
+        Debug.Log(previousScene + " --> " + currentScene);
+    }
+
+    private void Start() {
+        GameObject.DontDestroyOnLoad(PlayerManager.GetPlayer.gameObject);
+        GameObject.DontDestroyOnLoad(UIManager.UICanvas.gameObject);
+    }
+
 }
