2674e81d6611d9d9253f3e3830264aa9835d8d3c
Henry Lee <henry.lee@griffithuni.edu.au>
2023-08-09 14:16:04 +1000

    Update Player.cs
    Includes Health

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -9,13 +9,39 @@ public class Player : MonoBehaviour {
     [SerializeField] public float interactRange;
 
 
+    [SerializeField] public float startingHealth;
+    private float currentHealth;
+
+
     // Start is called before the first frame update
     void Start() {
-
+        currentHealth = startingHealth;
+        GameManager.Instance.UIManager.SetHPBarFill(GetHealthNormalized());
+        GameManager.Instance.UIManager.SetHPAmount((int)currentHealth);
     }
 
     // Update is called once per frame
     void Update() {
 
+    }
+
+
+    public void Damage(float damage) {
+
+        currentHealth -= damage;
+
+        if (currentHealth <= 0) {
+            currentHealth = 0;
+            Debug.Log("U died");
+            // Die
+        }
+
+        GameManager.Instance.UIManager.SetHPBarFill(GetHealthNormalized());
+        GameManager.Instance.UIManager.SetHPAmount((int)currentHealth);
+    }
+
+    public float GetHealthNormalized() {
+        float normalizedHealth = currentHealth / startingHealth;
+        return normalizedHealth;
     }
 }
