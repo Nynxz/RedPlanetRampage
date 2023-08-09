a981808635dd0b2ab4a002136fc61dd1330c87c7
Henry Lee <henry.lee@griffithuni.edu.au>
2023-08-09 13:06:40 +1000

    Update PlayerManager.cs

--- Assets/Scripts/Prototype/PlayerManager.cs
+++ Assets/Scripts/Prototype/PlayerManager.cs
@@ -1,36 +1,49 @@
 using StarterAssets;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using static GameManager;
+using static UnityEngine.InputSystem.InputSettings;
 
-public class PlayerManager : MonoBehaviour
-{
-    GameManager gameManager;
+public class PlayerManager : MonoBehaviour {
 
-    [SerializeField] private GameObject bullet;
-    [SerializeField] private Transform cameraRoot;
-    [SerializeField] private LayerMask interactMask;
+    private GameManager gameManager;
+    [SerializeField] private Player player;
 
-    [SerializeField]    [Range(0f, 5f)] private float interactRange;
-
-
-    private StarterAssetsInputs _input;
+    // Player Variables
+    private int currentMoney = 0;
+    private int currentScore = 0;
 
 
-    void Start()
-    {
-        gameManager = GameManager.Instance;
-        _input = GetComponent<StarterAssetsInputs>();
+    // Player Variable Update Events
+    public class UpdateAmmoArgs {
+        public int currentInMag;
+        public int maximumInMag;
+        public int ammoLeft;
     }
 
-    void Update()
-    {
-        if (GameManager.Instance.InputManager.interactInput) {
+    public event EventHandler<UpdateMoneyEventArgs> UpdateMoney;
+    public class UpdateMoneyEventArgs : EventArgs {
+        public int moneyAmount;
+    }
+
+    public event EventHandler<UpdateScoreEventArgs> UpdateScore;
+    public class UpdateScoreEventArgs : EventArgs {
+        public int scoreAmount;
+    }
+
+
+
+    void Start() {
+        gameManager = GameManager.Instance;
+        UpdateMoney += gameManager.UIManager.SetMoneyText;
+        UpdateScore += gameManager.UIManager.SetScoreText;
+    }
+
+    void Update() {
+        if (gameManager.InputManager.interactInput) {
             RaycastHit hit;
-            GameManager.Instance.InputManager.SetInteract(false);
-            if (Physics.Raycast(cameraRoot.position, cameraRoot.forward, out hit, interactRange, interactMask)) {
+            gameManager.InputManager.SetInteract(false);
+            if (Physics.Raycast(player.cameraRoot.position, player.cameraRoot.forward, out hit, player.interactRange, player.interactMask)) {
                 Debug.Log("Got Hit");
 
                 if (hit.collider.TryGetComponent(out Interactable interactable)) {
@@ -40,10 +53,41 @@ public class PlayerManager : MonoBehaviour
         }
     }
 
+    public void AddMoney(int amount) {
+        currentMoney += amount;
+        UpdateMoney?.Invoke(this, new UpdateMoneyEventArgs() {
+            moneyAmount = currentMoney
+        });
+    }
+    public void RemoveMoney(int amount) {
+        if (CheckMoneyAmount(amount)) {
+            currentMoney -= amount;
+            UpdateMoney?.Invoke(this, new UpdateMoneyEventArgs() {
+                moneyAmount = currentMoney
+            });
+        }
+    }
+    private bool CheckMoneyAmount(int amount) {
+        return amount <= currentMoney;
+    }
+
+    public void AddScore(int amount) {
+        currentScore += amount;
+        UpdateScore?.Invoke(this, new UpdateScoreEventArgs() {
+            scoreAmount = currentScore
+        });
+    }
+    public void RemoveScore(int amount) {
+        currentScore -= amount;
+        UpdateScore?.Invoke(this, new UpdateScoreEventArgs() {
+            scoreAmount = currentScore
+        });
+    }
+
     private void OnDrawGizmos() {
 
         // Interaction Probe
         Gizmos.color = Color.green;
-        Gizmos.DrawLine(cameraRoot.position, cameraRoot.position + cameraRoot.forward * interactRange);
+        Gizmos.DrawLine(player.cameraRoot.position, player.cameraRoot.position + player.cameraRoot.forward * player.interactRange);
     }
 }
